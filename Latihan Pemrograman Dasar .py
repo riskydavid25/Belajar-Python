a56aab3ba4eb200d374be2dba26ded9dea57a1d8
Risky David K <89583633+riskydavid25@users.noreply.github.com>
2021-09-13 13:18:05 +0700

    Add files via upload

---
+++ Latihan Pemrograman Dasar .py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Hello World!")
+
+
+# In[2]:
+
+
+print("Halo Dunia")
+print("Riset Bahasa Python")
+
+
+# In[3]:
+
+
+bilangan1 = 20
+bilangan2 = 10
+print(bilangan1 - bilangan2)
+
+
+# In[4]:
+
+
+contoh_list = ['Dewi','Budi','Cici','Linda','Cici']
+print(contoh_list)
+contoh_set = {'Dewi','Budi','Cici','Linda','Cici'}
+print(contoh_set)
+contoh_frozen_set = ({'Dewi','Budi','Cici','Linda','Cici'})
+print(contoh_frozen_set)
+
+
+# In[5]:
+
+
+person = {'nama' : 'Jumadi', 'pekerjaan' : 'Programmer'}
+print(person['nama'])
+print(person['pekerjaan'])
+
+
+# In[6]:
+
+
+contoh_list = [1, 'dua', 3, 4.0, 5]
+print(contoh_list[0])
+print(contoh_list[3])
+contoh_list = [1, 'dua', 3, 4.0, 5]
+contoh_list[3] = 'empat'
+print(contoh_list[3])
+
+
+# In[7]:
+
+
+contoh_list = [1, 'dua,', 3, 4.0, 5]
+print(contoh_list[0])
+print(contoh_list[3])
+contoh_list = [1, 'dua', 3, 4.0, 5]
+contoh_list[3] = 'empat'
+print(contoh_list[3])
+
+
+# In[ ]:
+
+
+
+
